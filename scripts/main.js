014d8a10405e26bab3f5346835fe07d1bdb4eed6
hanteliai <90987025+aloex118@users.noreply.github.com>
2022-12-28 23:10:16 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const start_btn = document.querySelector('start_btn');
+
+function redirect__1() {
+    document.location.href = 'test-page.html';
+}
---
+++ scripts/questions.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+    id: 1,
+    question: "Сколько лет Ивану?",
+    answer: "14",
+    options: [
+      "12",
+      "11",
+      "16",
+      "14"
+    ]
+  },
+
+    {
+    id: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+
+    {
+    id: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  
+    {
+    id: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  }
+];
